9c6b82bcaee1f6b8d36988dd5487e11e22d60d5e
conoha <conoha@users.noreply.github.com>
2019-08-16 07:42:32 +0900

    auto

--- views.py
+++ views.py
@@ -5,36 +5,67 @@ from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 import pandas as pd
 import matplotlib.pyplot as plt
-import io, os
+import io, os, tempfile
+import threading
 
 
-def getsvg():
-    abspath = os.path.dirname(os.path.abspath(__file__))
-    df = pd.read_csv(f'{abspath}/Temperature.csv', header=None)
+def getsvg(df):
     df.columns = ['time', 'temp', 'pressure', 'humidity']
     svg = ''
-    for name in ['temp', 'humidity', 'pressure']:
-        _, ax = plt.subplots(figsize=(15,3))
-        ax.plot([x[11:16] for x in df['time'][::-1]], df[name][::-1])
-        labels = ax.get_xticklabels()
-        plt.setp(labels, rotation=60, fontsize=10)
-        plt.setp(labels[1::6], rotation=60, fontsize=3)
-        plt.setp([x for i, x in enumerate(labels) if i % 6 != 0], rotation=60, fontsize=6)
-        plt.title(name)
-        buf = io.BytesIO()
+
+    _, ax1 = plt.subplots(figsize=(15,6))
+    ax1.plot([x[11:16] for x in df['time'][::-1]], df['temp'][::-1], label='temperature')
+    labels = ax1.get_xticklabels()
+    ax2 = ax1.twinx()
+    ax2.plot([x[11:16] for x in df['time'][::-1]], df['humidity'][::-1], color='purple', label='humidity')
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='center left')
+    plt.setp(labels, rotation=60, fontsize=10)
+    plt.setp(labels[1::6], rotation=60, fontsize=10)
+    plt.setp([x for i, x in enumerate(labels) if i % 6 != 1], rotation=60, fontsize=1)
+    plt.title('temperature and humidity')
+
+    with io.BytesIO() as buf:
         plt.savefig(buf, format='svg', bbox_inches='tight')
         s = buf.getvalue()
-        buf.close()
-        svg += s.decode('utf-8')
-        #plt.show()
+        
+    svg += s.decode('utf-8')
+    #plt.show()
+
+    _, ax3 = plt.subplots(figsize=(15,3))
+    ax3.plot([x[11:16] for x in df['time'][::-1]], df['pressure'][::-1], label='pressure')
+    labels = ax3.get_xticklabels()
+    plt.setp(labels, rotation=60, fontsize=10)
+    plt.setp(labels[1::6], rotation=60, fontsize=10)
+    plt.setp([x for i, x in enumerate(labels) if i % 6 != 1], rotation=60, fontsize=1)
+    plt.title('air pressure')
+
+    with io.BytesIO() as buf:
+        plt.savefig(buf, format='svg', bbox_inches='tight')
+        s = buf.getvalue()
+    #buf.close()
+    svg += s.decode('utf-8')
+
     return svg
 
+def write_html(data):
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    with tempfile.TemporaryDirectory() as dname:
+        x = f'{dname}/temp.txt'
+        with open(x, 'w') as f:
+            f.write(data)
+        df = pd.read_csv(x, header=None)
+    with open(f'{abspath}/Temperature.html', 'w') as f:
+        f.write(getsvg(df))    
+
 def temp(request):
+    abspath = os.path.dirname(os.path.abspath(__file__))
     data = request.GET.get(key="observationdata", default=None)
     if data is not None:
-        abspath = os.path.dirname(os.path.abspath(__file__))
-        with open(f'{abspath}/Temperature.csv', 'w') as f:
-            f.write(data)
+        t1 = threading.Thread(target=write_html, args=(data,))
+        t1.start()
         return HttpResponse('Done')
     else:
-        return HttpResponse(getsvg())
+        with open(f'{abspath}/Temperature.html', 'r') as f:
+            data = f.read()
+        return HttpResponse('<title>部屋の空気</title>' + data)
